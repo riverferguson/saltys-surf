efb2dafcbf2ea644da7471c7c870e2040c832658
River Ferguson <riverferguson@gmail.com>
2023-07-11 14:55:20 -0700

    add navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,37 +1,42 @@
-import { Link } from 'react-router-dom'
-import './index.css'
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const Nav = ({ user }) => {
-  
-    return (
-        <nav className="nav">
+  return (
+    <nav className="nav">
+      <Link to="/home" className="site-title">
+        <img
+          className="logo"
+          src={process.env.PUBLIC_URL + "/images/logo.jpg"}
+          alt="oops"
+        />
+        <span className="shop-name">&nbsp; Salty's Surfshop</span>
+      </Link>
+      <ul>
+        <li className="nav-link-wrapper">
+          <Link to="/products"> Products </Link>
+        </li>
 
-              <ul>
-                  <li className='nav-link-wrapper'>
-                  <Link to="/products"> Products </Link>
-                  </li>
+        <li className="nav-link-wrapper">
+          <Link to="/products/new">Sell</Link>
+        </li>
 
-                  <li className='nav-link-wrapper'>
-                  <Link to='/products/new'>Sell</Link>
-                  </li>
-                  
-                  <li className='nav-link-wrapper'>
-                  <Link to="/cartitems"> Cart </Link>
-                  </li>
+        <li className="nav-link-wrapper">
+          <Link to="/cartitems"> Cart </Link>
+        </li>
 
-                  <li className='nav-link-wrapper'>
-                  <Link to={user ? "/signout" : "/signin"}>
-                    {user ? "SignOut" : "SignIn"}
-                    </Link>
-                  </li>
+        <li className="nav-link-wrapper">
+          <Link to={user ? "/signout" : "/signin"}>
+            {user ? "SignOut" : "SignIn"}
+          </Link>
+        </li>
 
-                  <li className='nav-link-wrapper'>
-                    <Link to="/signup">SignUp</Link>
-                  </li>                                          
-                  
-              </ul>
-          </nav>
-        )
-    }
+        <li className="nav-link-wrapper">
+          <Link to="/signup">SignUp</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
