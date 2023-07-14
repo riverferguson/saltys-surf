4bebb844613b32c7c49ab1063db5207299f47ae8
River Ferguson <riverferguson@gmail.com>
2023-07-13 21:45:44 -0700

    style back to top button

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,40 +1,51 @@
-import React from 'react'
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-
-
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Footer = () => {
   return (
-        <>
-          <div className='icon-bar' style={{ backgroundColor: 'gray', color: 'white', padding: '25px' }}>
-            <a href='https://twitter.com' target="_blank" rel='noreferrer'>
-            <TwitterIcon className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
-            </a>
-            <a href='https://facebook.com' target="_blank" rel='noreferrer'>
-            <FacebookIcon className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
-            </a>
-            <a href='https://instagram.com' target="_blank" rel='noreferrer'>
-            <InstagramIcon className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
-            </a>
-            <a href='https://gmail.com/' target="_blank" rel='noreferrer'>
-            <MailOutlineIcon className='socials-icon' sx={{mr: 2, fontSize: 40, color: '#2196f3'}} />
-            </a>
-            <p>Salty's Surfshop &#8482;</p> 
-            <a href='#root'>
-            <Button variant="contained" size="medium">
-          Back To Top
-        </Button>
-            </a>
-       
+    <>
+      <div
+        className="icon-bar"
+        style={{ backgroundColor: "gray", color: "white", padding: "25px" }}
+      >
+        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+          <TwitterIcon
+            className="socials-icon"
+            sx={{ mr: 2, fontSize: 40, color: "#2196f3" }}
+          />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noreferrer">
+          <FacebookIcon
+            className="socials-icon"
+            sx={{ mr: 2, fontSize: 40, color: "#2196f3" }}
+          />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <InstagramIcon
+            className="socials-icon"
+            sx={{ mr: 2, fontSize: 40, color: "#2196f3" }}
+          />
+        </a>
+        <a href="https://gmail.com/" target="_blank" rel="noreferrer">
+          <MailOutlineIcon
+            className="socials-icon"
+            sx={{ mr: 2, fontSize: 40, color: "#2196f3" }}
+          />
+        </a>
+        <p>Salty's Surfshop &#8482;</p>
+        <a href="#root">
+          <Button variant="contained" size="medium" style={{marginLeft: '10px'}}>
+            Back To Top
+          </Button>
+        </a>
+      </div>
+    </>
+  );
+};
 
-          </div>
-        </>
-  )
-}
-
-export default Footer
+export default Footer;
